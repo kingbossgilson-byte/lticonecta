5c825513db364e63a3b6a00aca5c01d3e434aee4
kingbossgilson-byte <kingboss.gilson@gmail.com>
2026-02-24 18:44:23 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2');
+/* const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: 'localhost',
@@ -13,3 +13,21 @@ db.connect(err => {
 });
 
 module.exports = db;
+ */
+
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const db = mysql.createPool(process.env.DATABASE_URL);
+
+// Teste de conexão
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error('Erro ao conectar no MySQL:', err);
+  } else {
+    console.log('MySQL conectado 🚀');
+    connection.release();
+  }
+});
+
+module.exports = db.promise();
