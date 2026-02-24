b4396ace527e63b1616832a047d0ba6fbc6f575e
Gilson Rosa <kingboss.gilson@gmail.com>
2026-02-24 19:05:20 -0300

    db

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,4 @@
-const mysql = require('mysql2');
-
+/* const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -10,6 +9,20 @@ const db = mysql.createConnection({
 db.connect(err => {
   if (err) throw err;
   console.log('MySQL conectado');
+}); */
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const db = mysql.createPool(process.env.DATABASE_URL);
+
+// Teste de conexão
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error('Erro ao conectar no MySQL:', err);
+  } else {
+    console.log('MySQL conectado 🚀');
+    connection.release();
+  }
 });
 
-module.exports = db;
+module.exports = db.promise();
